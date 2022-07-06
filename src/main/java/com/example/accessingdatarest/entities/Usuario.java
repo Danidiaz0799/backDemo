e3c2095e50f3b887e2a80427f7b7a2412af938e4
Steven Diaz <90877963+Danidiaz0799@users.noreply.github.com>
2022-07-06 11:02:48 -0500

    Primer commit

---
+++ src/main/java/com/example/accessingdatarest/entities/Usuario.java
@@ -0,0 +1,59 @@
+package com.example.accessingdatarest.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String avatar;
+    private String gender;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return this.avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/main/java/com/example/accessingdatarest/repositories/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.example.accessingdatarest.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.accessingdatarest.entities.Usuario;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+@CrossOrigin(origins = "*")
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
+  List<Usuario> findByLastName(@Param("name") String name);
+}
